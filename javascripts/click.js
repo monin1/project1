a804c0f598daf84b33e2dbeb087a4076441f35a0
Alfonse12 <mudosamugisho@gmail.com>
2018-05-24 00:44:26 +0930

    Update click.js
    Javascript for form validation

--- javascripts/click.js
+++ javascripts/click.js
@@ -62,3 +62,162 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+
+
+
+//  Validating the contact us form  
+
+function validateForm() {  
+
+    var x = document.forms["inputForm"]["email"].value; 
+	var y = document.forms["inputForm"]["name"].value; 
+	var z = document.forms["inputForm"]["lname"].value;
+	
+    var n = x.indexOf("@"); 
+    var m = x.lastIndexOf(".");
+	
+	
+	if(y =="" && x == "" && z == ""){ 
+		alert("Please fill out all the field");
+		$("#name").focus();
+		$("#name").css({"background-color": "white"});
+		return false;
+	}
+	else if(y ==""){ 
+		alert("The name field cannot be left blanc. Please provide a name");
+		$("#name").focus();
+		return false;
+	}
+	else if(z ==""){
+		alert("Please select Last name");
+		$("#lname").focus();
+		return false;
+	}
+	else if(x ==""){ 
+		alert("Please type a Email address in the email field");
+		$("#email").focus();
+	
+		return false;
+	}
+	else if(n<1 || m<n+2 || m+2>= x.length){
+		alert("Not a valid email address. plesae type valid email address");
+		$("#email").focus();
+		return false;
+	}
+	else if (document.getElementById('feedback').value == ''){
+		alert("Can you please leave us a message in the textarea");
+		document.getElementById('feedback').focus();
+		return false;
+	}
+	
+	else{
+		alert("Your feedback has been submited. We may contact you soon for more information. Thank you !!! ");
+		$("#name,#email,#lname").css({"background-color": "white"});
+		return true;
+	} 
+}
+
+
+
+function validateForm2() {
+		
+            var isAllValid = true;
+            $('.errorgName').hide();
+            $('#gName').empty();
+            $('.container').removeClass('has-errorgName');
+			
+			if ($('#gName').val().trim() == "") {
+                $('#gName').focus();
+                $('#gName').siblings('.errorgName').show();
+                $('#gName').parents('.container').addClass('has-errorgName');
+                isAllValid = false;
+            }
+			else if ($('#gName').val().trim().length <= 1) {
+                $('#gName').focus();
+                $('#gName').siblings('.errorgName2').show();
+                $('#gName').parents('.container').addClass('has-errorgName2');
+                isAllValid = false;
+            }
+			
+			if ($('#userPass').val().trim() == "") {
+                $('#userPass').focus();
+                $('#userPass').siblings('.erroropassword').show();
+                $('#userPass').parents('.container').addClass('has-erroropassword');
+                isAllValid = false;
+            }
+			else if ($('#userPass').val().trim().length <= 7) {
+                $('#userPass').focus();
+                $('#userPass').siblings('.erroropassword3').show();
+                $('#userPass').parents('.container').addClass('has-erroropassword3');
+                isAllValid = false;
+            }
+			 
+            return isAllValid;
+ }
+ 
+ 
+ 
+ $(document).ready(function(){	
+$("#gName").focusout(function(){
+  if($(this).val() === ""){
+    $(".errorgName").show();
+  }else{
+    $(".errorgName").hide();
+  }
+});
+
+$("#gName").focusin(function(){
+  $(".errorgName").hide();
+});
+});
+
+// Error for event title size
+
+$(document).ready(function(){	
+$("#eventTitle").focusout(function(){
+  if($(this).val() === ""){
+    $(".errortitle2").hide();
+  }else{
+    $(".errortitle2").hide();
+  }
+});
+
+$("#eventTitle").focusin(function(){
+  $(".errortitle2").hide();
+});
+});
+
+
+ // error for password
+
+$(document).ready(function(){	
+$("#userPass").focusout(function(){
+  if($(this).val() === ""){
+    $(".erroropassword").show();
+  }else{
+    $(".erroropassword").hide();
+  }
+});
+
+$("#userPass").focusin(function(){
+  $(".erroropassword").hide();
+});
+});
+
+// error for passord length check
+
+$(document).ready(function(){	
+$("#userPass").focusout(function(){
+  if($(this).val() === ""){
+    $(".erroropassword3").hide();
+  }else{
+    $(".erroropassword3").hide();
+  }
+});
+
+$("#userPass").focusin(function(){
+  $(".erroropassword3").hide();
+});
+});
