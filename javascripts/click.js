da66d49e2ffec2607bcb906567d321a3566d2b9a
monin1 <36976263+monin1@users.noreply.github.com>
2018-05-24 17:36:17 +0930

    Update click.js

--- javascripts/click.js
+++ javascripts/click.js
@@ -46,18 +46,24 @@ $(document).ready(function() {
     }
   });
 });
-	/* backtotop */
-	// When the user scrolls down 20px from the top of the document, show the button
+
+/* backtotop */
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("myBtn").style.display = "block";
+	} else {
+			document.getElementById("myBtn").style.display = "none";
+	}
 } //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
 
 
 //  Validating the contact us form  
